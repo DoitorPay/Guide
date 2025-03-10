9dde52ab9ff4d8f4478060bc4cd95a4e4c3b1e1e
Jaerim Kim <jrkim@the-51.com>
2025-03-10 17:48:31 +0900

    퍼블리싱 목록 스타일링

--- frontend/src/guide/pubGuide.jsx
+++ frontend/src/guide/pubGuide.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState}  from "react"
 import { Link } from "react-router-dom";
+import '../assets/css/Guide.scss'
 
 function PubGuide() {
   const [items, setItems] = useState([]);
@@ -11,15 +12,20 @@ function PubGuide() {
       .catch((error) => console.error("데이터 불러오기 실패:", error));
   }, []);
   return (
-      <div>
-      <ul>
-        {items.map((item) => (
-          <li key={item.id}>
-            <Link to={item.path}>{item.title}</Link>
-            <span>파일명: {item.fileName}</span>
-          </li>
-        ))}
-      </ul>
+      <div className="inner-wrap">
+        <div className="pub-guide">
+          <h1>퍼블리싱 목록</h1>
+          <ul>
+            {items.map((item) => (
+              <li key={item.id}>
+                <h3>
+                  <Link to={item.path}>{item.title}</Link>
+                </h3>
+                <span>파일명: {item.fileName}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
   )
 }
