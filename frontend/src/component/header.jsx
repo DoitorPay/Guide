03aec5f3a2ebfb8442d04dfb9be6d3f5239bea09
Jaerim Kim <jrkim@the-51.com>
2025-03-10 13:47:39 +0900

    reset css 설정

---
+++ frontend/src/component/header.jsx
@@ -0,0 +1,22 @@
+import '../assets/css/Header.css'
+import '../assets/css/reset.css'
+
+function Header() {
+
+  return (
+    <>
+      <div>
+        <header>
+          <ul>
+            <li>Do it or Pay</li>
+            <li>스타일 가이드</li>
+            <li>컴포넌트</li>
+            <li>퍼블리싱 목록</li>
+          </ul>
+        </header>
+      </div>
+    </>
+  )
+}
+
+export default Header
