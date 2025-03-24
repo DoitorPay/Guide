5a48a2592ec14a85cbef5fc6b82beb23307496f0
HWIJAE KIM <dusan960766@gmail.com>
2025-03-24 17:52:34 +0900

    버튼 컴포넌트 수정

--- frontend/src/component/button.jsx
+++ frontend/src/component/button.jsx
@@ -3,16 +3,36 @@ import React from "react";
 // import { Link } from "react-router-dom";
 import "../assets/css/Button.scss";
 
-function Button() {
-    return(
-        <>
-        <div className="cmp-button">
+function Button({ 
+    buttonName = "Button", 
+    onClick, 
+    type = "button", 
+    disabled = false,
+    isLoading = false,
+    addClass = "",
+}) {
+    return (
+        <div className={`cmp-button ${addClass}`}>
             <div className="cmp-button__button-wrap">
-                <button>Test</button>
+                <button
+                    type={type}
+                    onClick={onClick}
+                    disabled={disabled || isLoading}
+                    className={isLoading ? "loading" : ""}
+                >
+                    {isLoading ? "Loading..." : buttonName}
+                </button>
             </div>
         </div>
-    </>
     );
 }
+// 사용 예시
+/* <Button 
+    buttonName="로그인" 
+    onClick={handleLogin} 
+    type="submit"
+    isLoading={isLoading}
+    disabled={!isValid}
+/> */
 
 export default Button;
