90e80485f41056cbe39b3031ec083de156131874
jinjintv <theffffff@naver.com>
2025-03-25 18:06:33 +0900

    style: frontGuide CSS fix

--- frontend/src/guide/cssGuide.jsx
+++ frontend/src/guide/cssGuide.jsx
@@ -1,7 +1,19 @@
 import React, { useState } from "react";
 
 function CssGuide() {
-  return <div className="inner-wrap">asdf</div>;
+  return(
+    <div className="inner-wrap">
+    <div className="pub-guide">
+    <h1>Style Guide</h1>
+      <h2>SCSS</h2>
+      <ul style={{border:'1px solid #bdbdbd', padding: '15px', borderRadius: '5px', margin: '10px 0 0 0'}}>
+        <li>asdf</li>
+        
+      </ul>
+    <br/>
+    </div>
+    </div>
+  );
 }
 
 export default CssGuide;
