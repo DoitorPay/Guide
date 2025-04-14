cc17971b2486d9e6f4413abf11362c79dea91000
ajbuwidnnfjwj <68640265+ajbuwidnnfjwj@users.noreply.github.com>
2025-04-14 22:07:30 +0900

    modi: 디버그를 위해 있던 print 삭제

--- backend/app/index.py
+++ backend/app/index.py
@@ -16,7 +16,6 @@ load_dotenv()
 NEO4J_URI = os.getenv("NEO4J_URI")
 NEO4J_USER = os.getenv("NEO4J_USERNAME")
 NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD")
-print(NEO4J_URI)
 driver = GraphDatabase.driver(NEO4J_URI, auth=(NEO4J_USER, NEO4J_PASSWORD))
 
 # 네임스페이스 정의
