966919419c0f914dff5ef4318414a72b7c8cae52
Hwijae kim <113835565+HwijaeKim@users.noreply.github.com>
2025-04-15 10:45:04 +0900

    Test: button 컴포넌트 onClick 속성 작동 테스트

--- frontend/src/guide/cmpButton.jsx
+++ frontend/src/guide/cmpButton.jsx
@@ -22,18 +22,18 @@ function cmpButton() {
 
                 <br></br><br></br>
                 <p className="guide-text-desc">type: primary</p>
-                <Button type="primary" buttonName="버튼 활성화"/> &nbsp;
+                <Button type="primary" onClick={guideClick} buttonName="버튼 활성화"/> &nbsp;
                 <Button type="primary" buttonName="버튼 비활성화" disabled />
 
                 <br></br>
 
                 <br></br><br></br>
                 <p className="guide-text-desc">type: secondary</p>
-                <Button type="secondary" buttonName="고객센터 연결"/> &nbsp;
+                <Button type="secondary" onClick={guideClick} buttonName="고객센터 연결"/> &nbsp;
 
                 <br></br>
                 <p className="guide-text-desc">type: close</p>
-                <Button type="close"/> &nbsp;
+                <Button type="close" onClick={guideClick} /> &nbsp;
             </div>
     );
 }
