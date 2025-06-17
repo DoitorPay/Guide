b5ddda5e0d0b1d7af1ef83aae9e309e4f9800e38
jaerim <jaerim1102@dfy.co.kr>
2025-06-17 17:42:16 +0900

    feat: 레이아웃 재사용 가능 컴포넌트로 수정

--- frontend/src/pages/mainLayout.jsx
+++ frontend/src/pages/mainLayout.jsx
@@ -3,33 +3,32 @@ import '@/assets/css/components/mainLayout.scss';
 import Navigation from '@/components/nav';
 import Header from "@/components/header"
 
-const MainLayout = () => {
+const MainLayout = ({ children }) => {
   return (
     <div className="wrapper">
-    <div className="main-layout">
-      <Header />
-      <section className="content-area">
-        <div className="content-box" />
-        <div className="content-box" />
-        <div className="content-box" />
-        <div className="content-box" />
-        <div className="content-box" />
-        <div className="content-box" />
-        <div className="content-box" />
-        <div className="content-box" />
-        <div className="content-box" />
-        <div className="content-box" />
-        <div className="content-box" />
-        <div className="content-box" />
-        <div className="content-box" />
-      </section>
-
-      <footer>
-        <Navigation/>
-      </footer>
-    </div>
+      <div className="main-layout">
+        <Header />
+        <section className="content-area">
+          {children}
+        </section>
+        <footer>
+          <Navigation />
+        </footer>
+      </div>
     </div>
   );
 };
 
+
 export default MainLayout;
+
+// 사용 방법!
+// import MainLayout from '@/components/MainLayout';
+
+// const 어쩌구 = () => (
+//   <MainLayout>
+//     <컴포넌트 />
+//     <컴포넌트 />
+//     <컴포넌트 />
+//   </MainLayout>
+// );
