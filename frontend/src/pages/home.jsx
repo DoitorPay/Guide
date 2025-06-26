a6406ec4085cead61eacc41a194f4f03a23944aa
jaerim <jaerim1102@dfy.co.kr>
2025-06-26 16:18:11 +0900

    markup: 홈 화면 마크업 1차

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import MainLayout from "@/pages/MainLayout";
 import HomeProfile from '@/components/homeProfile';
 import ProgressCard from '@/components/progressCard';
+import TodoList from '@/components/todoList';
+import GroupCard from "@/components/group/GroupCard";
+import WeekCalendar from "@/components/WeekCalendar";
+
 const MainPage = () => {
   return (
     <MainLayout
@@ -12,6 +16,14 @@ const MainPage = () => {
     >
         <HomeProfile/>
         <ProgressCard/>
+        <div>
+            {/* 전체보기 컴포넌트 만들어서 넣읍시다.. */}
+            <TodoList type='home'/>
+        </div>
+        <GroupCard />
+        <div>
+            <WeekCalendar type="default" />
+        </div>
     </MainLayout>
   );
 };
