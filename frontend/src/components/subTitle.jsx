f027cbf52b82ee1e7681c63250a22286df5c8b68
Hwijae kim <113835565+HwijaeKim@users.noreply.github.com>
2025-06-29 18:51:57 +0900

    modi: 서브타이틀 컴포넌트 week 타입 추가

--- frontend/src/components/subTitle.jsx
+++ frontend/src/components/subTitle.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { format } from 'date-fns';
+import { format, startOfWeek, endOfWeek } from 'date-fns';
 import { ko } from 'date-fns/locale';
 
 const SubTitle = ({ title, type = 'default', desc, link }) => {
-  const today = format(new Date(), 'MM월 dd일', { locale: ko });
+  // eslint-disable-next-line no-unused-vars
+  const currentDate = new Date();
+  const today = format(currentDate, 'M월 d일', { locale: ko });
+  
+
+  const weekStart = startOfWeek(currentDate, { weekStartsOn: 1, locale: ko });
+  const weekEnd = endOfWeek(currentDate, { weekStartsOn: 1, locale: ko });
+  const week = `${format(weekStart, 'M월 d일', { locale: ko })} ~ ${format(weekEnd, 'M월 d일', { locale: ko })}`;
 
   return (
     <div className={`sub-title sub-title--${type}`}>
@@ -17,6 +24,9 @@ const SubTitle = ({ title, type = 'default', desc, link }) => {
         )}
         {type === 'date' && (
           <span className="sub-title__date">{today}</span>
+        )}
+        {type === 'week' && (
+          <span className="sub-title__date">{week}</span>
         )}
 
       {type === 'desc' && desc && (
