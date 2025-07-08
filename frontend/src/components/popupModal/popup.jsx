a71ca09c808fe4c2af8b21eda9bbd168f5d9e4a9
Hwijae kim <113835565+HwijaeKim@users.noreply.github.com>
2025-07-08 22:00:49 +0900

    feat: 팝업 컴포넌트(작업중2)

--- frontend/src/components/popupModal/popup.jsx
+++ frontend/src/components/popupModal/popup.jsx
@@ -1,19 +1,69 @@
 import React from 'react';
+import Button from '@/components/button/button';
 
-const Popup = (
-    whatIcon,
+const Popup = ({
+    icon,
     title,
     subtitle,
     buttonName,
+    button2Name,
+    buttonAria,
+    button2Aria,
     onClick,
-) => {
+    onSecondClick,
+    setPopup = true,
+}) => {
     return (
-        <div className="popup">
-            <div className="popup__container">
-            <img src={`/icons/${whatIcon}.svg`} />
-            
-            </div>
-            <div className="popup__overlay"></div>
-        </div>
+        <>
+            {setPopup && (
+                <div className="popup">
+                    <div className="popup__container">
+                        <img src={`/icons/${icon}.svg`} />
+                        <div className="text-wrapper">
+                            <p className="text-wrapper-title">{title}</p>
+                            <p className="text-wrapper-subtitle">{subtitle}</p>
+                        </div>
+                        {
+                            !button2Name && (
+                                <div className="button-wrapper">
+                                    <Button
+                                        type="primary"
+                                        buttonName={buttonName}
+                                        aria={buttonAria}
+                                        onClick={onClick}
+                                        bgColor="var(--color-gray-scale-50)"
+                                        textColor="var(--Grayscale-Gray-700)"
+                                    />
+                                </div>
+
+                            )
+                        }
+                        {
+                            button2Name && (
+                                <div className="button-wrapper">
+                                    <Button
+                                        type="primary"
+                                        buttonName={buttonName}
+                                        aria={buttonAria}
+                                        onClick={onClick}
+                                        bgColor="var(--color-gray-scale-50)"
+                                        textColor="var(--Grayscale-Gray-700)"
+                                    />
+                                    <Button
+                                        type="primary"
+                                        buttonName={button2Name}
+                                        aria={button2Aria}
+                                        onClick={onSecondClick}
+                                    />
+                                </div>
+                            )
+                        }
+                    </div>
+                    <div className="overlay"></div>
+                </div>
+            )}
+        </>
     )
 }
+
+export default Popup;
---
+++ frontend/src/guide/popupGuide.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import Button from "@/components/button/button";
+import Popup from "@/components/popupModal/popup";
+
+const PopupGuide = () => {
+    const [setPopup, setSetPopup] = useState(false);
+    
+    function toggleSetPopup() {
+        setSetPopup(!setPopup);
+    }
+    return (
+        <div className="cmp-guide">
+            <p className="guide-text-title">
+            icon: 중앙에 들어갈 아이콘<br />
+            title: 메인 타이틀<br />
+            subtitle: 서브 타이틀<br />
+            buttonName: 버튼1 이름<br />
+            button2Name: 버튼2 이름(안 쓰면 자동으로 버튼1만 들어감)<br />
+            buttonAria: 버튼1 아리아<br />
+            button2Aria: 버튼2 아리아<br />
+            onClick: 버튼1 클릭했을 때<br />
+            onSecondClick: 버튼2 클릭했을 때<br />
+            </p>
+
+
+            <Button buttonName="팝업 띄우기" aria="팝업 띄우기" onClick={toggleSetPopup}/>
+            <Popup
+                icon="done-gray"
+                title="title입니다."
+                subtitle="subtitle입니다."
+                buttonName="버튼1"
+                button2Name="버튼2"
+                setPopup={setPopup}
+            />
+        </div>
+    )
+}
+
+export default PopupGuide;
