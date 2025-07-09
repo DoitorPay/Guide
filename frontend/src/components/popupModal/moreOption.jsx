ec8b671863a1765a99879c23e705068ed0ba53ed
Hwijae kim <113835565+HwijaeKim@users.noreply.github.com>
2025-07-10 00:52:46 +0900

    feat: 더보기 옵션 컴포넌트 (작업 중2)
    팝업 열렸을 때 바깥부분 클릭하면 다시 사라지는 거 구현해야 됨

--- frontend/src/components/popupModal/moreOption.jsx
+++ frontend/src/components/popupModal/moreOption.jsx
@@ -2,26 +2,32 @@ import React from "react";
 
 const MoreOption = ({
     title = "타이틀",
-    subtitle = "서브타이틀",
+    options = [],
+    isOpen = false,
 }) => {
-    
+    let clickOverlay = isOpen;
+    function closeModal() {
+        clickOverlay = false;
+    }
+
     return(
-        <div className="more-option">
-            <div className="more-option__container">
-                <div className="more-title">{title}</div>
-                <div className="more-subtitle">
-                <div className="more-subtitle--list">{subtitle}</div>
-                <div className="more-subtitle--list">{subtitle}</div>
-                <div className="more-subtitle--list">{subtitle}</div>
-                <div className="more-subtitle--list">{subtitle}</div>
-                <div className="more-subtitle--list">{subtitle}</div>
-                <div className="more-subtitle--list">{subtitle}</div>
+        <>
+            { isOpen && (
+                <div className="more-option">
+                    <div className="more-option__container">
+                        <div className="more-title">{title}</div>
+                        <div className="more-subtitle">
+                            {options.map((option, index) => (
+                                <div key={index} className="more-subtitle--list" onClick={option.onClick}>
+                                    {option.label}
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                    <div className="overlay" onClick={closeModal()}></div>
                 </div>
-            </div>
-
-            <div className="overlay"></div>
-        </div>
-
+            )}
+        </>
     )
 }
 
--- frontend/src/guide/moreOptionGuide.jsx
+++ frontend/src/guide/moreOptionGuide.jsx
@@ -3,36 +3,29 @@ import Button from "@/components/button/button";
 import MoreOption from "@/components/popupModal/moreOption";
 
 const MoreOptionGuide = () => {
-    const [setPopup, setSetPopup] = useState(false);
+    const [isMoreOptionOpen, setIsMoreOptionOpen] = useState(false);
     
-    function toggleSetPopup() {
-        setSetPopup(!setPopup);
+    function toggleMoreOption() {
+        setIsMoreOptionOpen(!isMoreOptionOpen);
     }
     return (
         <div className="cmp-guide">
             <p className="guide-text-title">
-            icon: 중앙에 들어갈 아이콘<br />
             title: 메인 타이틀<br />
-            subtitle: 서브 타이틀<br />
-            buttonName: 버튼1 이름<br />
-            button2Name: 버튼2 이름(안 쓰면 자동으로 버튼1만 들어감)<br />
-            buttonAria: 버튼1 아리아<br />
-            button2Aria: 버튼2 아리아<br />
-            onClick: 버튼1 클릭했을 때<br />
-            onSecondClick: 버튼2 클릭했을 때<br />
-            setPopup: 페이지에서 true/false 토글 함수를 생성해 조절
+            options: 배열 형태로 리스트를 작성<br />
             </p>
 
 
-            <Button buttonName="팝업 띄우기" aria="팝업 띄우기" onClick={toggleSetPopup}/>
+            <Button buttonName="팝업 띄우기" aria="팝업 띄우기" onClick={toggleMoreOption}/>
             <MoreOption
-                icon="done-gray"
-                title="title입니다."
-                subtitle="subtitle입니다."
-                buttonName="닫기"
-                onClick={toggleSetPopup}
-                button2Name="버튼2"
-                setPopup={setPopup}
+                title="타이틀입니다"
+                options={[
+                    { label: "월요일", onClick: () => console.log("월요일 클릭") },
+                    { label: "화요일", onClick: () => console.log("화요일 클릭") },
+                    { label: "수요일", onClick: () => console.log("수요일 클릭") },
+                ]}
+                isOpen={isMoreOptionOpen}
+                
             />
         </div>
     )
