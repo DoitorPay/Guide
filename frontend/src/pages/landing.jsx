fab95eb572d87a4ebc6971819532b82a046a4456
Hwijae kim <113835565+HwijaeKim@users.noreply.github.com>
2025-07-19 02:55:30 +0900

    markup: 랜딩페이지(시작화면) 작업 완료

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import LandingLayout from "@/pages/landingLayout";
+import KaKaoLoginRedirectButton from "@/components/button/kakaoButton";
+import NaverLoginRedirectButton from "@/components/button/NaverButton";
 
 const Landing = () => {
     return(
         <LandingLayout>
+            <div className="landing-wrapper">
+                <div className="visual-container">
+                    <img src="/src/assets/image/common/logo-white.png"/>
+                    <p className="title">죽음의 스터디를 시작한다</p>
+                </div>
 
+                <div className="signup-button-container">
+                    <KaKaoLoginRedirectButton />
+                    <NaverLoginRedirectButton />
+                </div>
+            </div>
         </LandingLayout>
     )
 }
