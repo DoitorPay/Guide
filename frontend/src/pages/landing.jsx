7ab8f96b4938f76d40ffa5c8d8586ce326389927
Hwijae kim <113835565+HwijaeKim@users.noreply.github.com>
2025-07-19 01:59:38 +0900

    markup: 랜딩페이지(시작화면) 생성

---
+++ frontend/src/pages/landing.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import LandingLayout from "@/pages/landingLayout";
+
+const Landing = () => {
+    return(
+        <LandingLayout>
+
+        </LandingLayout>
+    )
+}
+
+export default Landing;
