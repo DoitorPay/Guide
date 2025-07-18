d4e8600f828872a3cd01944b073c1acc450f41fc
Hwijae kim <113835565+HwijaeKim@users.noreply.github.com>
2025-07-19 02:54:39 +0900

    style: 네이버/카카오 로그인 버튼 스타일 작업

--- frontend/src/components/button/kakaoButton.jsx
+++ frontend/src/components/button/kakaoButton.jsx
@@ -9,8 +9,9 @@ const KaKaoLoginRedirectButton = () => {
         <button
               type="button"
               onClick={handleLogin}
-              className="bg-yellow-400 hover:bg-yellow-500 text-white font-semibold py-2 px-4 rounded transition"
+              className="signup-button kakao"
             >
+              <img src="/icons/kakao.svg"/>
               카카오 로그인
             </button>
     )
