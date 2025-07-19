d4a09a93fd0044ef5bda8ea1fbfd184077d67184
jaerim1102 <jaerim1102@naver.com>
2025-07-19 14:56:20 +0900

    프로필 카드 db 연동 중... 확인 필요

--- frontend/src/components/profile/ProfileCard.jsx
+++ frontend/src/components/profile/ProfileCard.jsx
@@ -1,27 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import ProfileImage from '@/components/profile/ProfileImage';
 import ProfileName from '@/components/profile/ProfileName';
 
-const ProfileCard = ({
-    name = '이름(닉네임)',
-    status = '아자아자 화이팅 ~ !',
-    imageSrc = '',
-    onClick,
-  }) => {
-    return (
-      <div className="profile-card" onClick={onClick}>
-        <ProfileImage src={imageSrc} size={100} className="profile-card__image" />
-        <div className="profile-card__info">
-          <ProfileName name={name} size="md" />
-          <span className="profile-card__status">{status}</span>
-        </div>
-        <img
-          src="/icons/arrow-right-orange.svg"
-          alt="arrow"
-          className="profile-card__arrow"
-        />
+const ProfileCard = ({ status = '아자아자 화이팅 ~!', onClick }) => {
+  const [nickname, setNickname] = useState('닉네임');
+  const [profileImage, setProfileImage] = useState('');
+
+  useEffect(() => {
+    axios.get('/api/user/nickname')
+      .then(res => {
+        setNickname(res.data.profile);
+      })
+      .catch(err => {
+        console.error('닉네임 불러오기 실패:', err);
+      });
+
+    axios.get('/api/user/profile_img')
+      .then(res => {
+        setProfileImage(res.data.profile);
+      })
+      .catch(err => {
+        console.error('프로필 이미지 불러오기 실패:', err);
+      });
+  }, []);
+
+  return (
+    <div className="profile-card" onClick={onClick}>
+      <ProfileImage src={profileImage} size={100} className="profile-card__image" />
+      <div className="profile-card__info">
+        <ProfileName name={nickname} size="md" />
+        <span className="profile-card__status">{status}</span>
       </div>
-    );
-  };
-  
-  export default ProfileCard;
+      <img
+        src="/icons/arrow-right-orange.svg"
+        alt="arrow"
+        className="profile-card__arrow"
+      />
+    </div>
+  );
+};
+
+export default ProfileCard;
