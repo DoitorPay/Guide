0a1ca0345cf0a99c295dcf1422666717ea0ebb9b
jaaerim <jrkim@the-51.com>
2025-07-22 15:42:35 +0900

    하트프로필에프로필연동

--- frontend/src/components/profile/heartProfile.jsx
+++ frontend/src/components/profile/heartProfile.jsx
@@ -1,18 +1,33 @@
-import React, { useState } from 'react';
-import ProfileImage from '@/components/profile/ProfileImage';
+import React from 'react';
+import UserProfileRow from '@/components/profile/UserProfileRow';
 import LikeButton from '@/components/button/likeButton';
 
-const HeartProfile = ({ avatar, user }) => {
-    return (
-        <div className="heart-profile" style={{display: 'flex', alignItems: 'flex-end', justifyContent: 'space-between'}}>
-            <div>
-                <ProfileImage src={avatar} size={46} />
-                <span className="heart-profile-name">ㅎㅇ</span> 
-                {/* 이거 추후에 닉+며칠전 컴포넌트로 교체해야함 진이가 만든거 */}
-            </div>
-            <LikeButton initialCount={0} initialLiked={false} filledHeartSrc="/icons/heart-filled.svg" emptyHeartSrc="/icons/heart-empty.svg" size={24} />
-        </div>
-    );
+const HeartProfile = ({
+  avatar = '',
+  user = '',
+  date = '',
+  liked = false,
+  likeCount = 0,
+}) => {
+  return (
+    <div className="heart-profile-wrapper">
+      <UserProfileRow
+        src={avatar}
+        name={user}
+        date={date}
+        variant="postMeta"
+        size={46}
+        nameWeight="semibold"
+      />
+      <LikeButton
+        initialLiked={liked}
+        initialCount={likeCount}
+        filledHeartSrc="/icons/heart-filled.svg"
+        emptyHeartSrc="/icons/heart-empty.svg"
+        size={24}
+      />
+    </div>
+  );
 };
 
 export default HeartProfile;
