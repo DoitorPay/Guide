3437cc3d66b84a75c96cb1dc7c1b0fc0bc52020f
HWIJAE KIM <dusan960766@gmail.com>
2025-07-22 10:44:58 +0900

    feat: 프로필 사진 변경 컴포넌트 기능구현 완료(API연동 전)

--- frontend/src/components/profile/changeProfileImage.jsx
+++ frontend/src/components/profile/changeProfileImage.jsx
@@ -1,23 +1,66 @@
 import React, { useRef, useState } from "react";
 import ProfileImage from '@/components/profile/profileImage';
+import MoreOption from "../popupModal/moreOption";
 
-const changeProfileImage = () => {
+const ChangeProfileImage = () => {
+    const [isMoreOptionOpen, setIsMoreOptionOpen] = useState(false); // MoreOption 열림/닫힘 상태
 
+    const defaultAvatar = '/images/default-avatar.png';
 
+    const fileInputRef = useRef(null);
 
-    const [changedImageSrc, setChangedImageSrc] = useState('');
+    const handleOptionSelect = (optionValue) => {
+        if (optionValue === "setDefault") {
+            setChangedImageSrc(defaultAvatar);
+        } else if (optionValue === "selectAlbum") {
+            fileInputRef.current.click();
+        }
+        setIsMoreOptionOpen(false);
+    };
+
+    const handleFileChange = (event) => {
+        const file = event.target.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                setChangedImageSrc(reader.result);
+            };
+            reader.readAsDataURL(file);
+        }
+    };
+
+    const changeOptions = [
+        { value: "setDefault", label: "기본 이미지", onClick: () => handleOptionSelect("setDefault") },
+        { value: "selectAlbum", label: "앨범에서 선택하기", onClick: () => handleOptionSelect("selectAlbum")  }
+    ]; 
+
+    const [changedImageSrc, setChangedImageSrc] = useState(defaultAvatar);
 
     return(
         <div className="change-profile-image">
-            <div className="edit-icon">
+            <ProfileImage 
+                size = '117px'
+                src = {changedImageSrc}
+                key = {changedImageSrc}
+            />
+                        <div className="edit-icon" onClick={() => setIsMoreOptionOpen(true)}>
                 <img src="/icons/ic-edit-orange.svg"/>
             </div>
-            <ProfileImage 
-                size = '117'
-                src = {changedImageSrc}
+            <input
+                type="file"
+                accept="image/*"
+                ref={fileInputRef}
+                onChange={handleFileChange}
+                style={{ display: 'none' }}
+            />
+            <MoreOption
+                title="프로필 사진 수정"
+                options={changeOptions}
+                isOpen={isMoreOptionOpen}
+                onClose={() => setIsMoreOptionOpen(false)}
             />
         </div>
     );
 }
 
-export default changeProfileImage;
+export default ChangeProfileImage;
