347b2eedef42f4aa23c53e8be478805fdcdcd060
Hwijae kim <113835565+HwijaeKim@users.noreply.github.com>
2025-07-28 21:07:09 +0900

    markup: 랜딩페이지 타이틀 문구 랜덤으로 나오도록 수정

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import LandingLayout from "@/pages/landingLayout";
 import KaKaoLoginRedirectButton from "@/components/button/kakaoButton";
 import NaverLoginRedirectButton from "@/components/button/NaverButton";
 
 const Landing = () => {
+    const landingTitle = [
+        "죽음의 스터디를 시작한다",
+        "오늘도 딱!대와 함께",
+        "벌칙을 피해라",
+        "딱!대와 함께 숨막히는 레이스"
+    ];
+
+    const randomLandingTitle = useState(() => {
+        const randomIndex = Math.floor(Math.random() * landingTitle.length);
+        return landingTitle[randomIndex];
+    });
+
     return(
         <LandingLayout>
             <div className="landing-wrapper">
                 <div className="visual-container">
                     <img src="/images/logo-white.png"/>
-                    <p className="title">죽음의 스터디를 시작한다</p>
+                    <p className="title">{randomLandingTitle}</p>
                 </div>
 
                 <div className="signup-button-container">
