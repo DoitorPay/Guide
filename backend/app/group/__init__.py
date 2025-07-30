e289cb133c0ada14eb3692b095abf0aa308b32f9
ajbuwidnnfjwj <68640265+ajbuwidnnfjwj@users.noreply.github.com>
2025-07-30 22:31:30 +0900

    그룹정보 요청시 맴버 리스트 추기

--- backend/app/group/__init__.py
+++ backend/app/group/__init__.py
@@ -1,5 +1,5 @@
 from flask import jsonify
-from flask_restx import Namespace, Resource, reqparse
+from flask_restx import Namespace, Resource, reqparse, fields
 
 from app.DB.NeoDriver import driver
 
@@ -10,18 +10,37 @@ parser = reqparse.RequestParser()
 parser.add_argument('id', type=str,
                     help='그룹 id')
 
+group_model = ns_group.model('Group', {
+    "category": fields.String,
+    "description": fields.String,
+    "duration": fields.String,
+    "gid": fields.String,
+    "name": fields.String,
+    "punish": fields.List(fields.String, example=["엉덩이로 이름쓰기", "노래부르기"]),
+    "todo": fields.List(fields.String, example=["개발하기", "게임하기"]),
+    "members": fields.List(fields.ClassName)
+})
+
 @ns_group.route('')
 class Group(Resource):
     @ns_group.expect(parser)
+    @ns_group.marshal_with(group_model)
     def get(self):
         gid = parser.parse_args().get('id')
         if not gid:
             return "요청한 그룹을 찾을 수 없습니다", 404
 
         with driver.session() as neo_session:
-            results = neo_session.run("MATCH(g:Group {gid: $gid}) return g", gid=gid)
-            response = []
-            for record in results:
-                response.append(dict(record["g"]))
+            results = neo_session.run("""
+                MATCH (p:Person)-[r]->(g:Group {gid: $gid})
+                return p, r, g
+            """, gid=gid)
+            response = dict(results.single()['g'])
+
+            results = neo_session.run("""
+                            MATCH (p:Person)-[r]->(g:Group {gid: $gid})
+                            return p, r, g
+                        """, gid=gid)
+            response["members"] = [dict(record['p']) for record in results]
 
             return jsonify(response)
