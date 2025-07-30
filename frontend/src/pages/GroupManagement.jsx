f8cd783a3c5361e601ec49b146c4ef6251ef0c21
Hwijae kim <113835565+HwijaeKim@users.noreply.github.com>
2025-07-30 21:34:13 +0900

    style&markup: 그룹 관리 페이지 스타일 + 일부 마크업 작업

--- frontend/src/pages/GroupManagement.jsx
+++ frontend/src/pages/GroupManagement.jsx
@@ -1,55 +1,106 @@
 import { React, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Input from '@/components/input/Input';
-import SubTitle from '@/components/subtitle/subTitle';
 import Button from '@/components/button/button.jsx';
-import MainLayout from '@/pages/MainLayout';
+import SignupLayout from '@/pages/SignupLayout';
 import ImageUploader from '@/components/group/ImageUploader';
 import MissionCount from '@/components/group/MissionCount';
+import Popup from '@/components/popupModal/popup';
+import TossGroupAdmin from '@/components/group/tossGroupAdmin';
 
 
 const GroupManagement = () => {
-    
+  const navigate = useNavigate();
+  const handleGoBack = () => {
+      navigate(-1);
+  };
+
+  // 해산, 위임 팝업 컨트롤
+  const [dissolvePopup, setDissolvePopup] = useState(false);
+  const [tossPopup, setTossPopup] = useState(false);
+
+  function toggleDissolvePopup() {
+      setDissolvePopup(!dissolvePopup);
+  };
+  function toggleTossPopup() {
+    setTossPopup(!tossPopup);
+  };
+
+  // 더미데이터, API연동
+  const members = [
+    { id: 1, name: "홍길동", profile: "/images/default-avatar.png" },
+    { id: 2, name: "홍길순", profile: "/images/default-avatar.png" },
+    { id: 3, name: "홍길차", profile: "/images/default-avatar.png" },
+    { id: 4, name: "홍길타", profile: "/images/default-avatar.png" },
+    { id: 5, name: "홍길오", profile: "/images/default-avatar.png" },
+    { id: 6, name: "홍길동", profile: "/images/default-avatar.png" },
+    { id: 7, name: "홍길순", profile: "/images/default-avatar.png" },
+    { id: 8, name: "홍길차", profile: "/images/default-avatar.png" },
+    { id: 9, name: "홍길타", profile: "/images/default-avatar.png" },
+    { id: 10, name: "홍길오", profile: "/images/default-avatar.png" },
+];
+
   return (
-    <MainLayout
-              headerProps={{
-                type: "default",
-                icon1: "notifications",
-              }}
-            >
-        <form className="group-management-form">
+    <SignupLayout
+      headerProps={{
+        title: "프로필",
+        type: "header-b",
+        icon1: "arrow-left",
+        icon1OnClick: handleGoBack }}
+    >
+      <form className="group-management-form">
 
-    <ImageUploader / >
-      <Input
-        label="그룹 이름"
-        name="groupName"
-        placeholder="기존 그룹 이름"
-        required
-      />
 
-      <Input
-        label="그룹 설명"
-        name="groupDescription"
-        placeholder="기존 그룹 설명"
-        required
-      />
-
-      <MissionCount />
-    <div className="form-terms">
-         <Button
-        type="default"
-        buttonName="그룹 해산"
+        <ImageUploader
+          label="그룹 대표 사진"
         />
-         <Button
-        type="default"
-        buttonName="그룹 운영 위임"
+        <Input
+          label="그룹 이름"
+          name="groupName"
+          placeholder="기존 그룹 이름"
+          required
         />
+
+        <Input
+          label="그룹 설명"
+          name="groupDescription"
+          placeholder="기존 그룹 설명"
+          required
+        />
+
+        <MissionCount />
+        <div className="form-terms">
+          <Button
+            type="third"
+            buttonName="그룹 해산"
+            onClick={toggleDissolvePopup}
+          />
+          <Button
+            type="third"
+            buttonName="그룹 운영 위임"
+            onClick={toggleTossPopup}
+          />
         </div>
         <Button
-        type="primary"
-        buttonName="생성 완료"
+          type="primary"
+          buttonName="생성 완료"
         />
-    </form>
-            </MainLayout>
+      </form>
+      <Popup
+          icon="error-gray"
+          title="그룹을 해산하시겠어요?"
+          subtitle="해산한 그룹은 다시 복구할 수 없어요."
+          buttonName="해산"
+          onSecondClick={toggleDissolvePopup}
+          button2Name="취소"
+          setPopup={dissolvePopup}
+        />
+        <TossGroupAdmin
+          members={members}
+          onClick={toggleTossPopup}
+          setPopup={tossPopup}
+        />
+    </SignupLayout>
   );
 };
 
