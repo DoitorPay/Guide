fabce03ef1a6a4496a7dac2531b1900c5ff80759
Hwijae kim <113835565+HwijaeKim@users.noreply.github.com>
2025-08-01 00:44:16 +0900

    feat: 프로필 카드 컴포넌트 상태메시지 DB에서 불러오도록 수정

--- frontend/src/components/profile/ProfileCard.jsx
+++ frontend/src/components/profile/ProfileCard.jsx
@@ -21,12 +21,37 @@ const ProfileCard = ({ onClick }) => {
     fetchNickname();
   }, []);
 
+      // 상태메시지 불러오기
+      const [quote, setQuote] = useState('');
+      useEffect(() => {
+          const fetchQuote = async() => {
+              try {
+                  const response = await fetch("http://localhost:8000/user/user_properties", {
+                      credentials: "include",
+                      });
+                  if (!response.ok) {
+                      throw new Error(`오류!: ${response.status}`);
+                  }
+                  const jsonData = await response.json();
+                  console.log("----- GET 데이터 -----");
+                  console.log(JSON.stringify(jsonData, null, 2));
+                  console.log("-------------------");
+                  setQuote(jsonData[0].quote || '');
+              } catch(error) {
+                  console.error('상태메시지 불러오기 실패:', error);
+                  setQuote('상태메시지를 불러올 수 없습니다.')
+              }
+          };
+  
+          fetchQuote();
+      }, []);
+
   return (
     <div className="profile-card" onClick={onClick}>
       <ProfileImage size={100} className="profile-card__image" />
       <div className="profile-card__info">
         <ProfileName name={nickname} size="md" />
-        <span className="profile-card__status">아자아자 화이팅 ~ !</span>
+        <span className="profile-card__status">{quote}</span>
       </div>
       <img
         src="/icons/arrow-right-orange.svg"
